bef5cf7a86545adb314a1934f44d0fda4bf9248f
Nicolas Minue <nicominue@gmail.com>
2022-04-11 14:32:56 -0300

    Agrego LoadingMore

--- PilarTecnoMobile-Actividad-1/src/api/index.js
+++ PilarTecnoMobile-Actividad-1/src/api/index.js
@@ -1,8 +1,9 @@
 const BASE_URL = "https://pokeapi.co/api/v2";
 export const IMG_URL = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
 
-export const getPokemonList = () => {
-    return fetch(`${BASE_URL}/pokemon/?limit=20`)
+export const getPokemonList = (next) => {
+    const url = next?next: `${BASE_URL}/pokemon/?limit=20`;
+    return fetch(url)
         .then(res => res.json())
         .then(data => {
         return Promise.resolve(data);
