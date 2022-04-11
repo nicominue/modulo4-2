655612d96c0812dffa3f4f658dfa8bcc86ab7cd7
Nicolas Minue <nicominue@gmail.com>
2022-04-11 19:19:53 -0300

    Mapas y Geolocalizacion

--- PilarTecnoMobile-Actividad-1/src/components/App.js
+++ PilarTecnoMobile-Actividad-1/src/components/App.js
@@ -1,12 +1,18 @@
 import "react-native-gesture-handler";
-import React, { Component, useCallback } from "react";
+import React, { Component, useCallback, useEffect} from "react";
 import { View, Text } from "react-native";
 import AppStack from "../routes/app";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { store } from "../Store";
+import { hasLocationPermission } from "../LocationPermission";
 
 export default App = () => {
+    
+  useEffect(() => {
+    hasLocationPermission()
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
--- PilarTecnoMobile-Actividad-1/src/screens/Mapa/index.js
+++ PilarTecnoMobile-Actividad-1/src/screens/Mapa/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useCallback, useState, useEffect} from "react";
+import React, { Component, useCallback, useState, useEffect } from "react";
 import {
   SafeAreaView,
   View,
@@ -7,10 +7,11 @@ import {
   Dimensions,
   Pressable,
   ImageBackground,
-  Image,
 } from "react-native";
+import { Icon, Image, Button } from "react-native-elements";
 import Header from "../../components/Header";
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker } from "react-native-maps";
+import Geolocation from "react-native-geolocation-service";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -22,28 +23,141 @@ const LATITUDE_DELTA = 0.00422;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default Mapa = (props) => {
-  const [ region, setRegion ] = useState({
-    latitude:LATITUDE,
-    longitude:LONGITUDE,
-    latitudeDelta:LATITUDE_DELTA,
+  const [mapType, setMapType] = useState("standard");
+
+  const [region, setRegion] = useState({
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    latitudeDelta: LATITUDE_DELTA,
     longitudeDelta: LONGITUDE_DELTA,
-  })
+  });
 
   useEffect(() => {
     // _getLocation();
-  }, [])
+  }, []);
 
-  onRegionChange = region => {
-    this.setState({ region });
+  onRegionChange = (region) => {
+    setRegion(region);
+  };
+
+  const fitCoordinates = async() => {
+    console.log('centrando mapa')
+    this._getLocation()
+    }
+
+  _getLocation = async () => {
+    await Geolocation.getCurrentPosition(
+      async (posicion) => {
+        const longitude = posicion.coords.longitude;
+        const latitude = posicion.coords.latitude;
+        this.mapRef.animateToRegion(
+          {
+            latitude,
+            longitude,
+            latitudeDelta: region.latitudeDelta,
+            longitudeDelta: region.longitudeDelta,
+          },
+          1000
+        );
+        setRegion({ region: { ...region, longitude, latitude } });
+        console.log(
+          "posicion actual... Latitud: " +
+            `${JSON.stringify(longitude)}` +
+            " latitud: " +
+            `${JSON.stringify(latitude)}`
+        );
+      },
+      (error) => {
+        console.log("");
+        console.log("");
+        console.log("");
+        console.log("");
+        console.log(error.code, error.message);
+      },
+      {
+        accuracy: {
+          android: "high",
+          ios: "best",
+        },
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+        forceRequestLocation: true,
+      }
+    );
+  };
+
+  const changeMapType = () => {
+    if (mapType === "standard") {
+      setMapType("satellite");
+    } else {
+      setMapType("standard");
+    }
   };
 
   return (
     <View style={styles.container}>
       <Header />
       <MapView
-      initialRegion={region}
-      style={styles.map}
-      />
+        initialRegion={region}
+        style={styles.map}
+        mapType={mapType}
+        onRegionChangeComplete={(region) => onRegionChange(region)}
+      ></MapView>
+      <View
+        style={{
+          position: "absolute",
+          zIndex: 2,
+          marginTop: height / 7,
+          right: "1%",
+          flexDirection: "row",
+          backgroundColor: "white",
+          borderRadius: 100,
+          width: width / 10,
+          alignSelf: "flex-end",
+          margin: 20,
+          marginRight: 30,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon
+          name="crosshairs"
+          type="font-awesome"
+          color="#8d2d84"
+          size={width / 10}
+          onPress={() => fitCoordinates()}
+        />
+      </View>
+      <View style={styles.markerFixed}>
+        <Image
+          style={styles.marker}
+          source={require("../../assets/images/location.png")}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          marginTop: 10,
+          top: 100,
+          position: "absolute",
+          width: "100%",
+        }}
+      >
+        <Button title="Change Map Type" onPress={() => changeMapType()} />
+      </View>
+
+      <SafeAreaView style={styles.footer}>
+        <Text style={styles.region}>
+          longitud:
+          {JSON.stringify(region.longitude)}
+          {"\n"}latitud:
+          {JSON.stringify(region.latitude)}
+        </Text>
+      </SafeAreaView>
     </View>
   );
 };
@@ -53,6 +167,12 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  footer: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    bottom: 30,
+    position: "absolute",
+    width: "100%",
   },
   buttonContent: {
     width: width / 3,
@@ -85,6 +205,23 @@ export const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     width,
     height,
-    alignSelf:'center'
-    },
+    alignSelf: "center",
+  },
+  markerFixed: {
+    left: "50%",
+    marginLeft: -24,
+    marginTop: -48,
+    position: "absolute",
+    top: "50%",
+  },
+  marker: {
+    height: 48,
+    width: 48,
+  },
+  region: {
+    color: "#fff",
+    lineHeight: 20,
+    margin: 20,
+    alignSelf: "center",
+  },
 });
